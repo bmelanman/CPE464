70e8698c4fc0e99df33d62a7a780cf2572020dda
Bryce Melander <brycemelander@gmail.com>
2023-05-08 12:50:00 -0700

    updated

--- project02_cpe464/test.c
+++ project02_cpe464/test.c
@@ -1,15 +1,8 @@
 
 #include <printf.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arm_neon.h>
-
-#define TEST 16
 
 int main(void) {
 
-    uint32_t var1 = 10101010;
-
-    printf("%f", TEST / (float32_t) var1);
+    printf("Hello, balls\n");
 
 }
