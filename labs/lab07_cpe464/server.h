98dc44ac251557fe074399521158d965811777b4
Bryce Melander <brycemelander@gmail.com>
2023-05-10 15:15:46 -0700

    updated

--- labs/lab07_cpe464/server.h
+++ labs/lab07_cpe464/server.h
@@ -1,8 +1,24 @@
-//
-// Created by Bryce Melander on 5/10/23.
-//
 
 #ifndef LAB07_CPE464_SERVER_H
 #define LAB07_CPE464_SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "gethostbyname.h"
+#include "networks.h"
+#include "safeUtil.h"
+
+#define MAXBUF 80
+
+void processClient(int socketNum);
+
+void checkArgs(int argc, char *argv[], int *port, float *errRate);
 
 #endif //LAB07_CPE464_SERVER_H
